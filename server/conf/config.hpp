e8602d3749287dcc1e60f20ed92f701180574664
wdavilaneto <wdavilaneto@gmail.com>
2015-03-04 00:29:51 -0300

    commit inicial

---
+++ server/conf/config.hpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   config.hpp
+ * Author: walter
+ *
+ * Created on November 29, 2014, 4:03 PM
+ */
+
+#ifndef CONFIG_HPP
+#define	CONFIG_HPP
+
+
+namespace conf {
+
+    class ServerConfiguration {
+        
+        
+    };
+
+}
+
+
+#endif	/* CONFIG_HPP */
+
---
+++ server/conf/syntax_shugar.hpp
@@ -0,0 +1,24 @@
+/* 
+ * File:   syntax_shugar.hpp
+ * Author: walter
+ *
+ * Created on December 11, 2014, 12:26 AM
+ */
+
+#ifndef SYNTAX_SHUGAR_HPP
+#define	SYNTAX_SHUGAR_HPP
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+#define foreach BOOST_FOREACH
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SYNTAX_SHUGAR_HPP */
+
